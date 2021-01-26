0435226d596ab46f01232639c5d06fdc06c720db
Sergei Khanin <sergei_khanin@epam.com>
2021-01-26 13:51:18 +0000

    Update README.md

---
+++ src/test/java/com/epam/lab/MathServiceTest.java
@@ -0,0 +1,92 @@
+package com.epam.lab;
+
+import com.epam.lab.service.MathService;
+import com.epam.lab.service.SimpleMathService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathServiceTest {
+
+    private static MathService mathService;
+
+    @BeforeAll
+    static void setup() {
+        mathService = new SimpleMathService();
+    }
+
+    @ParameterizedTest
+    @MethodSource("com.epam.lab.Providers#testCompare")
+    @DisplayName("Тест метода MathService.compare(int value1, int value2)")
+    void testCompare(int value1, int value2, int expected) {
+        assertEquals(expected, mathService.compare(value1, value2), "Для входных параметров: " + value1 + " " + value2);
+    }
+
+    @Test
+    @DisplayName("Тест метода MathService.maxFrom(int value1, int value2)")
+    void testMaxFrom() {
+        assertEquals(2, mathService.maxFrom(-10, 2), "Для входных параметров: " + -10 + " " + 2);
+    }
+
+    @Test
+    @DisplayName("Тест метода MathService.maxFrom(int[] values)")
+    void testMaxFromArray() {
+        int[] param = {1, 3, 5, 7, 8};
+        assertEquals(8, mathService.maxFrom(param), "Для входных параметров: " + Arrays.toString(param));
+    }
+
+    @ParameterizedTest
+    @MethodSource("com.epam.lab.Providers#testSumProvider")
+    @DisplayName("Тест метода MathService.sum(int[] values)")
+    void testSum(int[] param, int expected) {
+        assertEquals(expected, mathService.sum(param), "Для входных параметров: " + Arrays.toString(param));
+    }
+
+    @ParameterizedTest
+    @MethodSource("com.epam.lab.Providers#testGetEvenDigitsProvider")
+    @DisplayName("Тест метода MathService.getEvenDigits(int[] values)")
+    void testGetEvenDigits(int[] param, int[] expected) {
+        assertArrayEquals(expected, mathService.getEvenDigits(param), "Для входных параметров: " + Arrays.toString(param));
+    }
+
+    @ParameterizedTest
+    @MethodSource("com.epam.lab.Providers#testCalcFactorialProvider")
+    @DisplayName("Тест метода MathService.calcFactorial(int initialVal)")
+    void testCalcFactorial(int param, int expected) {
+        assertEquals(expected, mathService.calcFactorial(param), "Для входного параметра: " + param);
+    }
+
+    @ParameterizedTest
+    @MethodSource("com.epam.lab.Providers#testCalcFibonacciProvider")
+    @DisplayName("Тест метода MathService.calcFibonacci(int number)")
+    void testCalcFibonacci(int param, int expected) {
+        assertEquals(expected, mathService.calcFibonacci(param), "Для входного параметра: " + param);
+    }
+
+    @ParameterizedTest
+    @MethodSource("com.epam.lab.Providers#testSortProvider")
+    @DisplayName("Тест метода MathService.sort(int[] arr)")
+    void testSort(int[] param, int[] expected) {
+        assertArrayEquals(expected, mathService.sort(param), "Для входных параметров: " + Arrays.toString(param));
+    }
+
+    @ParameterizedTest
+    @MethodSource("com.epam.lab.Providers#testIsPrimaryProvider")
+    @DisplayName("Тест метода MathService.isPrimary(int number)")
+    void testIsPrimary(int param, boolean expected) {
+        assertEquals(expected, mathService.isPrimary(param), "Для входного параметра: " + param);
+    }
+
+    @ParameterizedTest
+    @MethodSource("com.epam.lab.Providers#testReverseArrayProvider")
+    @DisplayName("Тест метода MathService.reverseArray(int[] arr)")
+    void testIsPrimary(int[] param, int[] expected) {
+        assertArrayEquals(expected, mathService.reverseArray(param), "Для входных параметров: " + Arrays.toString(param));
+    }
+}
---
+++ src/test/java/com/epam/lab/Providers.java
@@ -0,0 +1,91 @@
+package com.epam.lab;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class Providers {
+
+    private Providers() {
+
+    }
+
+    public static Stream<Arguments> testCompare() {
+        return Stream.of(
+                arguments(-10, -1, -1),
+                arguments(0, 0, 0),
+                arguments(3, 3, 0),
+                arguments(33, 2, 1)
+        );
+    }
+
+    public static Stream<Arguments> testSumProvider() {
+        return Stream.of(
+                arguments(new int[]{}, 0),
+                arguments(new int[]{1, 2}, 3),
+                arguments(new int[]{-6, 5, -2, 78}, 75),
+                arguments(new int[]{5, -7, 3, 7, 3, -1, 4, 3}, 17)
+        );
+    }
+
+    public static Stream<Arguments> testGetEvenDigitsProvider() {
+        return Stream.of(
+                arguments(new int[]{}, new int[]{}),
+                arguments(new int[]{1, 2}, new int[]{2}),
+                arguments(new int[]{-6, 5, -2, 78}, new int[]{-6, -2, 78}),
+                arguments(new int[]{5, -7, 3, 7, 3, -1, 3}, new int[]{})
+        );
+    }
+
+    public static Stream<Arguments> testCalcFactorialProvider() {
+        return Stream.of(
+                arguments(5, 120),
+                arguments(0, 1),
+                arguments(8, 40320),
+                arguments(9, 362880)
+        );
+    }
+
+    public static Stream<Arguments> testCalcFibonacciProvider() {
+        return Stream.of(
+                arguments(0, 0),
+                arguments(1, 1),
+                arguments(2, 1),
+                arguments(3, 2),
+                arguments(4, 3),
+                arguments(5, 5),
+                arguments(7, 13),
+                arguments(11, 89)
+        );
+    }
+
+    public static Stream<Arguments> testSortProvider() {
+        return Stream.of(
+                arguments(new int[]{}, new int[]{}),
+                arguments(new int[]{-1, -3, 4, 8, 5, 22, -5}, new int[]{-5, -3, -1, 4, 5, 8, 22}),
+                arguments(new int[]{3, 4, 3, 4}, new int[]{3, 3, 4, 4})
+        );
+    }
+
+    public static Stream<Arguments> testIsPrimaryProvider() {
+        return Stream.of(
+                arguments(2, true),
+                arguments(4, false),
+                arguments(5, true),
+                arguments(10, false),
+                arguments(21, false),
+                arguments(23, true),
+                arguments(7349, true)
+        );
+    }
+
+    public static Stream<Arguments> testReverseArrayProvider() {
+        return Stream.of(
+                arguments(new int[]{}, new int[]{}),
+                arguments(new int[]{-1, -3, 4, 8, 5, 22, -5}, new int[]{-5, 22, 5, 8, 4, -3, -1}),
+                arguments(new int[]{4, 4, 5, 5, 2, 6}, new int[]{6, 2, 5, 5, 4, 4})
+        );
+    }
+}
